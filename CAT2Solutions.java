2f3c3ab2c12ed8effa63d694f0d1237b35184276
JothishKamal <jothishkamal@gmail.com>
2026-03-20 15:49:31 +0530

    feat: implement buildFromLevelOrder method for TreeSolutions class

--- CAT2Solutions.java
+++ CAT2Solutions.java
@@ -16,6 +16,33 @@ class TreeNode {
 class TreeSolutions {
     TreeNode first, second, prev;
 
+    public TreeNode buildFromLevelOrder(Integer[] arr) {
+        if (arr == null || arr.length == 0)
+            return null;
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (i < arr.length) {
+            TreeNode parent = queue.poll();
+
+            if (arr[i] != null) {
+                parent.left = new TreeNode(arr[i]);
+                queue.add(parent.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != null) {
+                parent.right = new TreeNode(arr[i]);
+                queue.add(parent.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
     public void recoverTree(TreeNode root) {
         first = second = null;
         prev = new TreeNode(Integer.MIN_VALUE);
